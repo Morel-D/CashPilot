dfcb2d8165d05a262c5859a89900b32a70d3a6a4
Morel-D <morelDen@gmail.com>
2026-07-02 08:46:18 +0100

    redis setup with docker

--- server/src/main/java/com/example/server/ServerApplication.java
+++ server/src/main/java/com/example/server/ServerApplication.java
@@ -1,13 +1,30 @@
 package com.example.server;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.RedisTemplate;
+
 
 @SpringBootApplication
 public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner testRedis(RedisTemplate<String, Object> redisTemplate) {
+		return arg -> {
+			try {
+				redisTemplate.opsForValue().set("test:key", "Hello from CashPilot");
+				String value = (String) redisTemplate.opsForValue().get("test:key");
+				System.out.println("✅ Redis Connected Successfully! Value: "+ value);
+			}catch (Exception e){
+				System.err.println("❌ Redis Connection Failed: "+ e.getMessage());
+			}
+		};
 	}
 
 }
